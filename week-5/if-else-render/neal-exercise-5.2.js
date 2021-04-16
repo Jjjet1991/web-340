fde0a73c25e5ab9373298f46b9247f279994b44d
Jourdan Neal <jjjet1991@gmail.com>
2021-04-16 03:56:06 -0700

    Adding 5.2

---
+++ week-5/if-else-render/neal-exercise-5.2.js
@@ -0,0 +1,31 @@
+//Add require statements.
+var express = require ('express');
+var http = require('http');
+var path = require('path');
+
+//Add app functions.
+var app = express();
+//Telling Express to look in views files for files.
+app.set('views', path.resolve(__dirname, 'views'));
+//Use the EJS engine viewer.
+app.set('view engine', 'ejs');
+
+//Create Princess array.
+var k = [
+    "Belle",
+    "Ariel",
+    "Snow White",
+    "Aurora",
+    "Cinderella"
+];
+//Request and response statement calling princess using variable k.
+app.get("/", function(req,res){
+    res.render('index',{
+        princesses:k
+    });
+});
+
+//Create a server.
+http.createServer(app).listen(8080, function(){
+    console.log('Application is running on port 8080.');
+})
